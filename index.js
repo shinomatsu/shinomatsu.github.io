8a3b983846d35e8e0fc8eab3d9fe6212f8448e1c
shinomatsu <shi968k5@gmail.com>
2017-10-04 16:17:44 +0800

    Add index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+
+$(document).ready(function(){
+	$('#blue_btn').on("click",function(){
+		$('body').css('background','#C2EEFF')
+	})
+	$('#red_btn').on("click",function(){
+		$('body').css('background','#FFBEDA')
+	})
+	$('#green_btn').on("click",function(){
+		$('body').css('background','#B1F9D0')
+	})
+})
